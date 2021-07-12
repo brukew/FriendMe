3aa2efd57e9080b5c97c1f2144d66e3f5b458b7d
Bruke Wossenseged <brukew@fb.com>
2021-07-12 10:13:16 -0700

    Initial commit

---
+++ Pods/Target Support Files/Pods-FriendMe-FriendMeUITests/Pods-FriendMe-FriendMeUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FriendMe_FriendMeUITests : NSObject
+@end
+@implementation PodsDummy_Pods_FriendMe_FriendMeUITests
+@end
---
+++ Pods/Target Support Files/Pods-FriendMe-FriendMeUITests/Pods-FriendMe-FriendMeUITests-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_FriendMe_FriendMeUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_FriendMe_FriendMeUITestsVersionString[];
+
